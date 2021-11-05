9556bcfc4427a48c0797465970d4341676e1999e
clever eziogor <clevereziogor@clevers-MacBook-Pro.local>
2021-11-06 00:49:24 +0100

    todo form, and undex feild for render

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Form from '../pages/component/Form';
+import Header from '../pages/component/Header';
+import TodoList from '../pages/component/TodoList';
 
+const useStyles = makeStyles({
+    root: {
+        textAlign: "center",
+        // height: "100%"
+    },
+    form:{
+      marginTop: '1rem'
+    }
+});
 
 export default function Home() {
-  
+  const classes = useStyles();
   return (
-      <div></div>
+      <div className={classes.root}>
+          <Header />
+          <div className={classes.form}>
+          <Form />
+          </div>
+          <TodoList />
+      </div>
   
   )
 }
